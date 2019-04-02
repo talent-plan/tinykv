24d17905b0cadcc013b94e994e9718f23d708bba
xiaobogaga <m18202787110@gmail.com>
2019-04-02 11:31:49 +0800

    Port splitCheck and splitCheckObserver implementations, step 1 (#162)

--- tikv/raftstore/coprocessor.go
+++ tikv/raftstore/coprocessor.go
@@ -41,13 +41,229 @@ type splitChecker interface {
 	onKv(_ *observerContext, _ splitCheckKeyEntry) bool
 
 	/// splitKeys returns the desired split keys.
-	splitKeys() [][]byte
+	getSplitKeys() [][]byte
 
 	/// approximateSplitKeys returns the split keys without scan.
 	approximateSplitKeys(_ *metapb.Region, _ *badger.DB) ([][]byte, error)
 
 	/// policy returns the policy.
 	policy() pdpb.CheckPolicy
+}
+
+type sizeSplitChecker struct {
+	maxSize         uint64
+	splitSize       uint64
+	currentSize     uint64
+	splitKeys       [][]byte
+	batchSplitLimit uint64
+	checkPolicy     pdpb.CheckPolicy
+}
+
+func newSizeSplitChecker(maxSize, splitSize, batchSplitLimit uint64, policy pdpb.CheckPolicy) *sizeSplitChecker {
+	return &sizeSplitChecker{
+		maxSize:         maxSize,
+		splitSize:       splitSize,
+		batchSplitLimit: batchSplitLimit,
+		checkPolicy:     policy,
+	}
+}
+
+func (checker *sizeSplitChecker) onKv(obCtx *observerContext, spCheckKeyEntry splitCheckKeyEntry) bool {
+	//Todo, currently it is a place holder
+	return false
+}
+
+func (checker *sizeSplitChecker) getSplitKeys() [][]byte {
+	//Todo, currently it is a place holder
+	return nil
+}
+
+func (checker *sizeSplitChecker) approximateSplitKeys(region *metapb.Region, db *badger.DB) ([][]byte, error) {
+	//Todo, currently it is a place holder
+	return nil, nil
+}
+
+func (checker *sizeSplitChecker) policy() pdpb.CheckPolicy {
+	return checker.checkPolicy
+}
+
+type sizeSplitCheckObserver struct {
+	regionMaxSize uint64
+	splitSize     uint64
+	splitLimit    uint64
+	router        *router
+}
+
+func newSizeSplitCheckObserver(regionMaxSize, splitSize, splitLimit uint64, router *router) *sizeSplitCheckObserver {
+	return &sizeSplitCheckObserver{
+		regionMaxSize: regionMaxSize,
+		splitSize:     splitSize,
+		splitLimit:    splitLimit,
+		router:        router,
+	}
+}
+
+func (observer *sizeSplitCheckObserver) start() {}
+
+func (observer *sizeSplitCheckObserver) stop() {}
+
+func (observer *sizeSplitCheckObserver) addChecker(obCtx *observerContext, host *splitCheckerHost,
+	db *badger.DB, policy pdpb.CheckPolicy) {
+	//todo, currently it is just a placeholder
+}
+
+type halfSplitChecker struct {
+	buckets        [][]byte
+	curBucketSize  uint64
+	eachBucketSize uint64
+	checkPolicy    pdpb.CheckPolicy
+}
+
+func newHalfSplitCheck(eachBucketSize uint64, policy pdpb.CheckPolicy) *halfSplitChecker {
+	return &halfSplitChecker{eachBucketSize: eachBucketSize, checkPolicy: policy}
+}
+
+func (checker *halfSplitChecker) onKv(obCtx *observerContext, spCheckKeyEntry splitCheckKeyEntry) bool {
+	//Todo, currently it is a place holder
+	return false
+}
+
+func (checker *halfSplitChecker) getSplitKeys() [][]byte {
+	//Todo, currently it is a place holder
+	return nil
+}
+
+func (checker *halfSplitChecker) approximateSplitKeys(region *metapb.Region, db *badger.DB) ([][]byte, error) {
+	//Todo, currently it is a place holder
+	return nil, nil
+}
+
+func (checker *halfSplitChecker) policy() pdpb.CheckPolicy {
+	return checker.checkPolicy
+}
+
+type halfSplitCheckObserver struct {
+	halfSplitBucketSize uint64
+}
+
+func newHalfSplitCheckObserver(regionSizeLimit uint64) *halfSplitCheckObserver {
+	//todo, currently it is a place holder
+	return nil
+}
+
+func (observer *halfSplitCheckObserver) start() {}
+
+func (observer *halfSplitCheckObserver) stop() {}
+
+func (observer *halfSplitCheckObserver) addChecker(obCtx *observerContext, host *splitCheckerHost, db *badger.DB,
+	policy pdpb.CheckPolicy) {
+	//todo, currently it is just a placeholder
+}
+
+type keysSplitChecker struct {
+	maxKeysCount    uint64
+	splitThreshold  uint64
+	currentCount    uint64
+	splitKeys       [][]byte
+	batchSplitLimit uint64
+	checkPolicy     pdpb.CheckPolicy
+}
+
+func newKeysSplitChecker(maxKeysCount, splitThreshold, batchSplitLimit uint64,
+	policy pdpb.CheckPolicy) *keysSplitChecker {
+	return &keysSplitChecker{
+		maxKeysCount:    maxKeysCount,
+		splitThreshold:  splitThreshold,
+		batchSplitLimit: batchSplitLimit,
+		checkPolicy:     policy,
+	}
+}
+
+func (checker *keysSplitChecker) onKv(obCtx *observerContext, spCheckKeyEntry splitCheckKeyEntry) bool {
+	//Todo, currently it is a place holder
+	return false
+}
+
+func (checker *keysSplitChecker) getSplitKeys() [][]byte {
+	//Todo, currently it is a place holder
+	return nil
+}
+
+func (checker *keysSplitChecker) approximateSplitKeys(region *metapb.Region, db *badger.DB) ([][]byte, error) {
+	return nil, nil
+}
+
+func (checker *keysSplitChecker) policy() pdpb.CheckPolicy {
+	return checker.checkPolicy
+}
+
+type keysSplitCheckObserver struct {
+	regionMaxKeys   uint64
+	splitKeys       uint64
+	batchSplitLimit uint64
+	router          *router
+}
+
+func newKeysSplitCheckObserver(regionMaxKeys, splitKeys, batchSplitLimit uint64,
+	router *router) *keysSplitCheckObserver {
+	return &keysSplitCheckObserver{
+		regionMaxKeys:   regionMaxKeys,
+		splitKeys:       splitKeys,
+		batchSplitLimit: batchSplitLimit,
+		router:          router,
+	}
+}
+
+func (observer *keysSplitCheckObserver) start() {}
+
+func (observer *keysSplitCheckObserver) stop() {}
+
+func (observer *keysSplitCheckObserver) addChecker(obCtx *observerContext, host *splitCheckerHost, db *badger.DB,
+	policy pdpb.CheckPolicy) {
+	//todo, currently it is just a placeholder
+}
+
+type tableSplitChecker struct {
+	firstEncodedTablePrefix []byte
+	splitKey                []byte
+	checkPolicy             pdpb.CheckPolicy
+}
+
+func newTableSplitChecker(firstEncodedTablePrefix, splitKey []byte, policy pdpb.CheckPolicy) *tableSplitChecker {
+	return &tableSplitChecker{
+		firstEncodedTablePrefix: firstEncodedTablePrefix,
+		splitKey:                splitKey,
+		checkPolicy:             policy,
+	}
+}
+
+func (checker *tableSplitChecker) onKv(obCtx *observerContext, spCheckKeyEntry splitCheckKeyEntry) bool {
+	//Todo, currently it is a place holder
+	return false
+}
+
+func (checker *tableSplitChecker) getSplitKeys() [][]byte {
+	//Todo, currently it is a place holder
+	return nil
+}
+
+func (checker *tableSplitChecker) approximateSplitKeys(region *metapb.Region, db *badger.DB) ([][]byte, error) {
+	return nil, nil
+}
+
+func (checker *tableSplitChecker) policy() pdpb.CheckPolicy {
+	return checker.checkPolicy
+}
+
+type tableSplitCheckObserver struct{}
+
+func (observer *tableSplitCheckObserver) start() {}
+
+func (observer *tableSplitCheckObserver) stop() {}
+
+func (observer *tableSplitCheckObserver) addChecker(obCtx *observerContext, host *splitCheckerHost, db *badger.DB,
+	policy pdpb.CheckPolicy) {
+	//todo, currently it is just a placeholder
 }
 
 type splitCheckerHost struct {
@@ -73,7 +289,7 @@ func (spCheckerHost *splitCheckerHost) onKv(region *metapb.Region, spCheKeyEntry
 
 func (spCheckerHost *splitCheckerHost) splitKeys() [][]byte {
 	for _, checker := range spCheckerHost.checkers {
-		keys := checker.splitKeys()
+		keys := checker.getSplitKeys()
 		if len(keys) != 0 {
 			return keys
 		}
