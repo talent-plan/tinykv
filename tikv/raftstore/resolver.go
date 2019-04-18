0d97db63ab142cae8a865192d576718e88289455
disksing <i@disksing.com>
2019-04-18 12:04:14 +0800

    port resolver (#181)
    Signed-off-by: disksing <i@disksing.com>

--- tikv/raftstore/resolver.go
+++ tikv/raftstore/resolver.go
@@ -1,17 +1,58 @@
 package raftstore
 
-import "github.com/ngaut/unistore/pd"
+import (
+	"context"
+	"time"
+
+	"github.com/ngaut/unistore/pd"
+	"github.com/pingcap/errors"
+	"github.com/pingcap/kvproto/pkg/metapb"
+)
+
+const storeAddressRefreshSeconds = 60
+
+type storeAddr struct {
+	addr       string
+	lastUpdate time.Time
+}
 
 type resolverRunner struct {
-	pdClient pd.Client
+	pdClient   pd.Client
+	storeAddrs map[uint64]storeAddr
 }
 
 func newResolverRunner(pdClient pd.Client) *resolverRunner {
 	return &resolverRunner{
-		pdClient: pdClient,
+		pdClient:   pdClient,
+		storeAddrs: make(map[uint64]storeAddr),
 	}
 }
 
 func (r *resolverRunner) run(t task) {
-	// TODO
+	data := t.data.(resolveAddrTask)
+	data.callback(r.getAddr(data.storeID))
+}
+
+func (r *resolverRunner) getAddr(id uint64) (string, error) {
+	if sa, ok := r.storeAddrs[id]; ok {
+		if time.Since(sa.lastUpdate).Seconds() < storeAddressRefreshSeconds {
+			return sa.addr, nil
+		}
+	}
+	store, err := r.pdClient.GetStore(context.TODO(), id)
+	if err != nil {
+		return "", err
+	}
+	if store.GetState() == metapb.StoreState_Tombstone {
+		return "", errors.Errorf("store %d has been removed", id)
+	}
+	addr := store.GetAddress()
+	if addr == "" {
+		return "", errors.Errorf("invalid empty address for store %d", id)
+	}
+	r.storeAddrs[id] = storeAddr{
+		addr:       addr,
+		lastUpdate: time.Now(),
+	}
+	return addr, nil
 }
