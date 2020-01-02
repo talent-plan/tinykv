2844178cfde11eb23a6f0ba92f3d974fdd0b530f
Connor <zbk602423539@gmail.com>
2020-01-02 23:01:02 +0800

    add more proto

--- generate_go.sh
+++ generate_go.sh
@@ -2,17 +2,6 @@
 
 set -ex
 
-check_protoc_version() {
-    version=$(protoc --version)
-    major=$(echo ${version} | sed -n -e 's/.*\([0-9]\{1,\}\)\.[0-9]\{1,\}\.[0-9]\{1,\}.*/\1/p')
-    minor=$(echo ${version} | sed -n -e 's/.*[0-9]\{1,\}\.\([0-9]\{1,\}\)\.[0-9]\{1,\}.*/\1/p')
-    if [ "$major" -eq 3 ] && [ "$minor" -ge 8 ]; then
-        return 0
-    fi
-    echo "protoc version not match, version 3.1.x is needed, current version: ${version}"
-    return 1
-}
-
 push () {
     pushd $1 >/dev/null 2>&1
 }
@@ -24,10 +13,6 @@ pop () {
 cmd_exists () {
     which "$1" 1>/dev/null 2>&1
 }
-
-if ! check_protoc_version; then
-	exit 1
-fi
 
 PROGRAM=$(basename "$0")
 
