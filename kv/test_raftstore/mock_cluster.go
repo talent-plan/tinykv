e8f1cac1c15689361394f7c2b208ba5d021593cd
andylokandy <andylokandy@hotmail.com>
2020-01-27 01:33:48 +0800

    add bootstrap
    Signed-off-by: andylokandy <andylokandy@hotmail.com>

--- kv/test_raftstore/mock_cluster.go
+++ kv/test_raftstore/mock_cluster.go
@@ -20,63 +20,98 @@ import (
 )
 
 type MockCluster struct {
+	id       uint64
 	trans    *MockTransport
 	pdClient pd.Client
 	count    int
+	engines  map[uint64]*engine_util.Engines
 	nodes    map[uint64]*raftstore.Node
 }
 
-func NewMockCluster(pdClient pd.Client, count int) MockCluster {
+func NewMockCluster(id uint64, count int, pdClient pd.Client) MockCluster {
 	trans := NewMockTransport()
 	return MockCluster{
+		id:       id,
 		trans:    &trans,
 		count:    count,
 		pdClient: pdClient,
+		engines:  make(map[uint64]*engine_util.Engines),
+		nodes:    make(map[uint64]*raftstore.Node),
 	}
 }
 
 func (c *MockCluster) Start() error {
-	for nodeID := 0; nodeID < c.count; nodeID++ {
-		err := c.addNode(uint64(nodeID))
+	regionID := uint64(1)
+	ctx := context.TODO()
+	firstRegion := &metapb.Region{
+		Id:       regionID,
+		StartKey: []byte{},
+		EndKey:   []byte{},
+		RegionEpoch: &metapb.RegionEpoch{
+			Version: raftstore.InitEpochVer,
+			ConfVer: raftstore.InitEpochConfVer,
+		},
+	}
+
+	for storeID := uint64(1); storeID <= uint64(c.count); storeID++ {
+		dbPath, err := ioutil.TempDir("", "test-raftstore")
 		if err != nil {
 			return err
 		}
+		kvPath := filepath.Join(dbPath, "kv")
+		raftPath := filepath.Join(dbPath, "raft")
+		snapPath := filepath.Join(dbPath, "snap")
+
+		os.MkdirAll(kvPath, os.ModePerm)
+		os.MkdirAll(raftPath, os.ModePerm)
+		os.Mkdir(snapPath, os.ModePerm)
+
+		conf := config.DefaultConf
+		raftConf := tikvConf.NewDefaultConfig()
+		raftConf.SnapPath = snapPath
+
+		raftDB := createDB("raft", &conf.Engine)
+		kvdb := createDB("kv", &conf.Engine)
+		engine := engine_util.NewEngines(kvdb, raftDB, kvPath, raftPath)
+
+		c.engines[storeID] = engine
+
+		var wg sync.WaitGroup
+		pdWorker := worker.NewWorker("pd-worker", &wg)
+
+		router, batchSystem := raftstore.CreateRaftBatchSystem(raftConf)
+		raftRouter := raftstore.NewRaftstoreRouter(router) // TODO: init with local reader
+		snapManager := snap.NewSnapManager(raftConf.SnapPath)
+		node := raftstore.NewNode(batchSystem, &metapb.Store{}, raftConf, c.pdClient)
+
+		err = raftstore.BootstrapStore(engine, c.id, storeID)
+		if err != nil {
+			return err
+		}
+
+		peerID := storeID
+		peer := &metapb.Peer{
+			Id:      peerID,
+			StoreId: storeID,
+		}
+		_, err = raftstore.PrepareBootstrap(engine, storeID, regionID, peerID)
+		if err != nil {
+			return err
+		}
+		firstRegion.Peers = append(firstRegion.Peers, peer)
+
+		err = node.Start(ctx, engine, c.trans, snapManager, pdWorker, raftRouter)
+		if err != nil {
+			return err
+		}
+
+		c.nodes[storeID] = node
 	}
-	return nil
-}
 
-func (c *MockCluster) addNode(nodeID uint64) error {
-	dbPath, err := ioutil.TempDir("", "test-raftstore")
-	kvPath := filepath.Join(dbPath, "kv")
-	raftPath := filepath.Join(dbPath, "raft")
-	snapPath := filepath.Join(dbPath, "snap")
-
-	os.MkdirAll(kvPath, os.ModePerm)
-	os.MkdirAll(raftPath, os.ModePerm)
-	os.Mkdir(snapPath, os.ModePerm)
-
-	conf := config.DefaultConf
-	raftConf := tikvConf.NewDefaultConfig()
-	raftConf.SnapPath = snapPath
-
-	raftDB := createDB("raft", &conf.Engine)
-	kvdb := createDB("kv", &conf.Engine)
-	engines := engine_util.NewEngines(kvdb, raftDB, kvPath, raftPath)
-
-	var wg sync.WaitGroup
-	pdWorker := worker.NewWorker("pd-worker", &wg)
-
-	router, batchSystem := raftstore.CreateRaftBatchSystem(raftConf)
-	raftRouter := raftstore.NewRaftstoreRouter(router) // TODO: init with local reader
-	snapManager := snap.NewSnapManager(raftConf.SnapPath)
-	node := raftstore.NewNode(batchSystem, &metapb.Store{}, raftConf, c.pdClient)
-
-	err = node.Start(context.TODO(), engines, c.trans, snapManager, pdWorker, raftRouter)
+	_, err := c.nodes[1].BootstrapCluster(ctx, c.engines[1], firstRegion)
 	if err != nil {
 		return err
 	}
-
-	c.nodes[nodeID] = node
 
 	return nil
 }
