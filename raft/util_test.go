42d471f9242a2fc2d579581fc06e4f4ec3f7acf4
NingLin-P <linningde25@gmail.com>
2020-02-18 19:59:51 +0800

    raft: cleanup node (#65)
    * remove optimisticUpdate
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * remove MaxSizePerMsg
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * rename
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * remove node
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * remove MsgUnreachable
    
    Signed-off-by: linning <linningde25@gmail.com>
    
    * remove MustSync
    
    Signed-off-by: linning <linningde25@gmail.com>

--- raft/util_test.go
+++ raft/util_test.go
@@ -59,7 +59,6 @@ func TestIsLocalMsg(t *testing.T) {
 	}{
 		{pb.MessageType_MsgHup, true},
 		{pb.MessageType_MsgBeat, true},
-		{pb.MessageType_MsgUnreachable, true},
 		{pb.MessageType_MsgSnapStatus, true},
 		{pb.MessageType_MsgTransferLeader, false},
 		{pb.MessageType_MsgPropose, false},
