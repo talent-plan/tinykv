598f47683a1351249c705c6813daf26ad72843fd
Connor1996 <zbk602423539@gmail.com>
2020-03-10 23:25:16 +0800

    add comment for message and proto
    Signed-off-by: Connor1996 <zbk602423539@gmail.com>

--- kv/raftstore/message/msg.go
+++ kv/raftstore/message/msg.go
@@ -9,23 +9,44 @@ import (
 type MsgType int64
 
 const (
-	MsgTypeNull                  MsgType = 0
-	MsgTypeStart                 MsgType = 1
-	MsgTypeTick                  MsgType = 2
-	MsgTypeRaftMessage           MsgType = 3
-	MsgTypeRaftCmd               MsgType = 4
-	MsgTypeApplyRes              MsgType = 5
-	MsgTypeGcSnap                MsgType = 7
-	MsgTypeSplitRegion           MsgType = 8
-	MsgTypeRegionApproximateSize MsgType = 9
+	// just a placeholder
+	MsgTypeNull MsgType = 0
+	// message to start the ticker of peer
+	MsgTypeStart MsgType = 1
+	// message of base tick to drive the ticker
+	MsgTypeTick MsgType = 2
+	// message wraps a raft message that should be forwardded to Raft module
+	// the raft message is from peer on other store
+	MsgTypeRaftMessage MsgType = 3
+	// message warps a raft command that maybe a read/write request or admin request
+	// the raft command should be proposed to Raft module
+	MsgTypeRaftCmd MsgType = 4
+	// message to trigger split region
+	// it first asks Scheduler for allocating new split region's ids, then schedules a
+	// MsyTypeRaftCmd with split admin command
+	MsgTypeSplitRegion MsgType = 5
+	// message to update region approximate size
+	// it is sent by split checker
+	MsgTypeRegionApproximateSize MsgType = 6
+	// message to trigger gc generated snapshots
+	MsgTypeGcSnap MsgType = 7
+	// message of apply result from apply worker
+	MsgTypeApplyRes MsgType = 8
 
+	// message wraps a raft message to the peer not existing on the Store.
+	// It is due to region split or add peer conf change
 	MsgTypeStoreRaftMessage MsgType = 101
-	MsgTypeStoreTick        MsgType = 106
-	MsgTypeStoreStart       MsgType = 107
+	// message of store base tick to drive the store ticker, including store heartbeat
+	MsgTypeStoreTick MsgType = 106
+	// message to start the ticker of store
+	MsgTypeStoreStart MsgType = 107
 
+	// message to apply committed entires
 	MsgTypeApplyCommitted MsgType = 301
-	MsgTypeApplyRefresh   MsgType = 302
-	MsgTypeApplyProposal  MsgType = 303
+	// message to refresh apply worker region state
+	MsgTypeApplyRefresh MsgType = 302
+	// message to send proposal's callback to apply worker
+	MsgTypeApplyProposal MsgType = 303
 )
 
 type Msg struct {
