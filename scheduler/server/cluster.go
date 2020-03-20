0eecd7db6651c3525c9a794a65567f295a352bb1
Yang Keao <keao.yang@yahoo.com>
2020-03-20 14:24:04 +0800

    pick master change and add comment
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- scheduler/server/cluster.go
+++ scheduler/server/cluster.go
@@ -278,6 +278,8 @@ func (c *RaftCluster) handleStoreHeartbeat(stats *schedulerpb.StoreStats) error 
 
 // processRegionHeartbeat updates the region information.
 func (c *RaftCluster) processRegionHeartbeat(region *core.RegionInfo) error {
+	// Your Code Here(3C).
+
 	return nil
 }
 
