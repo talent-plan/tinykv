8835de0d78ac8a9aa1413150440f613aaf88b97a
charstal <charstal_h@outlook.com>
2021-11-22 18:47:27 +0800

    finished project1

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,8 +1,13 @@
 package standalone_storage
 
 import (
+	"os"
+	"path/filepath"
+
+	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -10,11 +15,20 @@ import (
 // communicate with other nodes and all data is stored locally.
 type StandAloneStorage struct {
 	// Your Data Here (1).
+	engines *badger.DB
+	config  *config.Config
 }
 
 func NewStandAloneStorage(conf *config.Config) *StandAloneStorage {
 	// Your Code Here (1).
-	return nil
+	dbPath := conf.DBPath
+	kvPath := filepath.Join(dbPath, "kv")
+
+	os.Mkdir(kvPath, os.ModePerm)
+
+	kvDB := engine_util.CreateDB(kvPath, false)
+
+	return &StandAloneStorage{engines: kvDB, config: conf}
 }
 
 func (s *StandAloneStorage) Start() error {
@@ -24,15 +38,35 @@ func (s *StandAloneStorage) Start() error {
 
 func (s *StandAloneStorage) Stop() error {
 	// Your Code Here (1).
+
+	if err := s.engines.Close(); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
 	// Your Code Here (1).
-	return nil, nil
+	txn := s.engines.NewTransaction(false)
+	reader := NewReader(txn)
+	return reader, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	wbatch := new(engine_util.WriteBatch)
+	for _, m := range batch {
+		switch m.Data.(type) {
+		case storage.Put:
+			put := m.Data.(storage.Put)
+			wbatch.SetCF(put.Cf, put.Key, put.Value)
+		case storage.Delete:
+			delete := m.Data.(storage.Delete)
+			wbatch.DeleteCF(delete.Cf, delete.Key)
+		}
+	}
+	if err := wbatch.WriteToDB(s.engines); err != nil {
+		return err
+	}
 	return nil
 }
