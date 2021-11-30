790c7fce27927e3ec47807bc888159ed44c85340
Gnomeshgh <tanghe994@gmail.com>
2021-11-30 19:52:00 +0800

    pro2 update log, tag:v2

--- raft/log.go
+++ raft/log.go
@@ -14,7 +14,10 @@
 
 package raft
 
-import pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+import (
+	pb "github.com/pingcap-incubator/tinykv/proto/pkg/eraftpb"
+	"log"
+)
 
 // RaftLog manage the log entries, its struct look like:
 //
@@ -48,7 +51,7 @@ type RaftLog struct {
 	stabled uint64
 
 	// all entries that have not yet compact.
-	// 压缩到存储的日志数量
+	// 未压缩到存储的日志数量
 	entries []pb.Entry
 
 	// the incoming unstable snapshot, if any.
@@ -57,11 +60,13 @@ type RaftLog struct {
 	pendingSnapshot *pb.Snapshot
 
 	// Your Data Here (2A).
-	// 陈旧的数组会被清除掉，firstIndex是未被清理的最小日志index，即applied == firstIndex -1
+	//unstable *unstable
+	// 陈旧的数组会被清除掉，firstIndex是未被清理的最小日志index，初始化时即applied == firstIndex -1
 	firstIndex uint64
-	//lastIndex  uint64 == stabled ??
-	term   uint64
-	length uint64
+	lastIndex  uint64
+	// offset
+	//offset     uint64
+	//term   uint64
 }
 
 // newLog returns log using the given storage. It recovers the log
@@ -69,28 +74,40 @@ type RaftLog struct {
 func newLog(storage Storage) *RaftLog {
 	// Your Code Here (2A).
 	// get first & last Index
-	firstIndex, _ := storage.FirstIndex()
-	lastIndex, _ := storage.LastIndex()
+	if storage == nil {
+		log.Panic("storage must not be nil")
+	}
+
+	firstIdx, err := storage.FirstIndex()
+	if err != nil {
+		panic(err)
+	}
+
+	lastIdx, err := storage.LastIndex()
+	if err != nil {
+		panic(err)
+	}
 
 	// get hardState info
-	hardState, _, _ := storage.InitialState()
+	hardState, _, err := storage.InitialState()
+	if err != nil {
+		panic(err)
+	}
 
-	// get entries [l,r)
-	ents, _ := storage.Entries(firstIndex, lastIndex+1)
-
-	// return a new log
+	// get entries [l,r) ，并不包含首个snap_log
+	ents, err := storage.Entries(firstIdx, lastIdx+1)
+	if err != nil {
+		panic(err)
+	}
+	// return a new log`
 	return &RaftLog{
-		storage:   storage,
-		committed: hardState.GetCommit(),
-		// TODO：why entry[0].index+1 ?
-		// 日志从1开始，存储的index从0开始？
-		applied:         firstIndex - 1,
-		stabled:         lastIndex,
+		storage:         storage,
+		committed:       hardState.GetCommit(),
+		applied:         firstIdx - 1, // 初始化时候，entries[0].index == applyIndex
+		stabled:         lastIdx,      // 初始化时候，从storage中回复的日志都是stabled状态
 		entries:         ents,
 		pendingSnapshot: nil,
-		firstIndex:      firstIndex,
-		term:            hardState.GetTerm(),
-		length:          uint64(len(ents)),
+		firstIndex:      firstIdx,
 	}
 }
 
@@ -101,89 +118,102 @@ func (l *RaftLog) maybeCompact() {
 	// Your Code Here (2C).
 }
 
+//  snapshot/first.....applied....committed....stabled.....last
+//  --------|------------------------------------------------|
 // unstableEntries return all the unstable entries
 func (l *RaftLog) unstableEntries() []pb.Entry {
 	// Your Code Here (2A).
-	if l.Length() > 0 {
-		length := l.stabled - l.FirstIndex() + 1
-		if length < 0 || length > l.Length() {
+	if len(l.entries) > 0 {
+		dis := l.stabled - l.FirstIndex() + 1
+		if dis < 0 || dis > uint64(len(l.entries)) {
 			return nil
 		}
-		return l.entries[length:]
+		return l.entries[dis:]
 	}
 	return nil
 }
 
+//  snapshot/first.....applied....committed....stabled.....last
+//  --------|------------------------------------------------|
 // nextEnts returns all the committed but not applied entries
 func (l *RaftLog) nextEnts() (ents []pb.Entry) {
 	// Your Code Here (2A).
-	if len(l.entries) > 0 {
-		commitLength := l.committed - l.FirstIndex() + 1
-		applyLength := l.applied - l.FirstIndex() + 1
-		if commitLength < 0 || applyLength > l.Length() {
+	count := uint64(len(l.entries))
+	if count > 0 {
+		commitDst := l.committed - l.FirstIndex() + 1
+		applyDst := l.applied - l.FirstIndex() + 1
+		if commitDst < 0 || commitDst > count || applyDst < 0 || applyDst > count || commitDst < applyDst {
 			return nil
 		}
-		if applyLength >= 0 && commitLength <= l.Length() {
-			// TODO::
-			return l.entries[applyLength:commitLength]
-		}
+		// update commitDst ==> commitDst+1
+		return l.entries[applyDst : commitDst+1]
 	}
+
 	return nil
 }
 
 // LastIndex return the last index of the log entries
 func (l *RaftLog) LastIndex() uint64 {
 	// Your Code Here (2A).
-	if l.Length() == 0 {
+	count := uint64(len(l.entries))
+	if count == 0 {
 		// find on storage
-		index, _ := l.storage.LastIndex()
-		return index
+		idx, err := l.storage.LastIndex()
+		if err != nil {
+			panic(err)
+		}
+		return idx
 	}
-	return l.entries[l.length-1].Index
+	return l.entries[count-1].Index
 }
 
 // Term return the term of the entry in the given index
 func (l *RaftLog) Term(i uint64) (uint64, error) {
 	// Your Code Here (2A).
-	if l.Length() > 0 {
-		currFirstIndex := l.FirstIndex()
-		if i >= currFirstIndex {
-			index := i - currFirstIndex
-			if index >= l.Length() {
+	// the valid term range is [index of dummy entry, last index]
+	dummyIdx := l.firstIndex - 1
+	if i < dummyIdx {
+		return 0, nil
+	} else if i > l.lastIndex {
+		return 0, ErrInvalidIndex
+	}
+
+	// find on entries
+	count := uint64(len(l.entries))
+	if count > 0 {
+		firstIdx := l.FirstIndex()
+		if i >= firstIdx {
+			indexDst := i - firstIdx
+			if indexDst >= count {
 				return 0, ErrUnavailable
-			} else {
-				return l.entries[index].Term, nil
 			}
-
-		} else {
-			return 0, ErrInvalidIndex
+			return l.entries[indexDst].Term, nil
 		}
+		// i < firstIdx maybe i == dummyIdx need find on storage
 	}
 
-	// l.Length == 0
-	// TODO:question
+	// TODO:question i < firstIdx maybe i == dummyIdx need find on storage
 	term, err := l.storage.Term(i)
-	if err == ErrUnavailable && !IsEmptySnap(l.pendingSnapshot) {
-		if i < l.pendingSnapshot.Metadata.Index {
-			err = ErrCompacted
-		}
+	if err == nil {
+		return term, nil
 	}
-	return term, err
+
+	if err == ErrCompacted || err == ErrUnavailable {
+		return 0, nil
+	}
+	panic(err)
 }
 
+// log 中的entry并不包含快照
 func (l *RaftLog) FirstIndex() uint64 {
-	if l.Length() == 0 {
+	if len(l.entries) == 0 {
 		// find in storage
-		index, _ := l.storage.FirstIndex()
-		return index - 1
+		// 存储中的firstIndex就是出快照之后的首份log
+		idx, err := l.storage.FirstIndex()
+		if err != nil {
+			panic(err)
+		}
+		return idx
 	}
 	return l.entries[0].Index
-}
-
-func (l *RaftLog) Length() uint64 {
-	// sync length
-	if uint64(len(l.entries)) != l.length {
-		l.length = uint64(len(l.entries))
-	}
-	return l.length
 }
