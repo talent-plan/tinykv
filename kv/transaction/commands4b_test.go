b3bb260d27549ed325f7f0f94947cfa6aea051e3
Hongyu Yang <93068857+yanghy233@users.noreply.github.com>
2024-10-11 19:55:00 +0800

    Correct the test cases to prevent the TestCommitMissingPrewrite4B case from not being tested. (#459)

--- kv/transaction/commands4b_test.go
+++ kv/transaction/commands4b_test.go
@@ -569,9 +569,9 @@ func TestCommitConflictRepeat4B(t *testing.T) {
 	})
 }
 
-// TestCommitMissingPrewrite4a tests committing a transaction which was not prewritten (i.e., a request was lost, but
+// TestCommitMissingPrewrite4B tests committing a transaction which was not prewritten (i.e., a request was lost, but
 // the commit request was not).
-func TestCommitMissingPrewrite4a(t *testing.T) {
+func TestCommitMissingPrewrite4B(t *testing.T) {
 	builder := newBuilder(t)
 	cmd := builder.commitRequest([]byte{3})
 	builder.init([]kv{
